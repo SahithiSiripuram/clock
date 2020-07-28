12df8ab4ced6e0ea67d9d4951977439ba8eeebe6
Sahithi Siripuram <sahithisiripuram1001@gmail.com>
2020-07-28 19:54:57 +0530

    Update canvas.js

--- frontend/canvas.js
+++ frontend/canvas.js
@@ -16,15 +16,15 @@ function drawFace(ctx, radius) {
   ctx.fillStyle = 'white';
   ctx.fill();
   grad = ctx.createRadialGradient(0,0,radius*0.95, 0,0,radius*1.05);
-  grad.addColorStop(0, '#333');
+  grad.addColorStop(0, '#000000');
   grad.addColorStop(0.5, 'white');
-  grad.addColorStop(1, '#333');
+  grad.addColorStop(1, '#000000');
   ctx.strokeStyle = grad;
   ctx.lineWidth = radius*0.1;
   ctx.stroke();
   ctx.beginPath();
   ctx.arc(0, 0, radius*0.1, 0, 2*Math.PI);
-  ctx.fillStyle = '#333';
+  ctx.fillStyle = '#000000';
   ctx.fill();
 }
 function drawNumbers(ctx, radius) {
